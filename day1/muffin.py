37ca826eed6c827e12489f64f983aef00289dd41
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-15 01:35:08 +1000

    Create muffin.py

---
+++ day1/muffin.py
@@ -0,0 +1,8 @@
+print("🧁Welcome to Muffinworld!🧁")
+print("its time to pick a muffiname🥳\n ")
+x=input("What is the name of the city you grew up in?")
+
+y=input("What is the name of your favorite cartoon character!\n")
+
+print( x + "" + y + ' ,This is your brand new muffin name.')
+print("🍰")
