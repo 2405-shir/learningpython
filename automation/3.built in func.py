fc99fd6fe837d854117ecb57e14e17a8432b776c
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-21 17:13:47 +1000

    Add files via upload

---
+++ automation/3.built in func.py
@@ -0,0 +1,12 @@
+#modules called standard library
+#before using functions in moule need to import module
+import random
+random.randint(1,10)
+#picks some random number between one to ten
+#needs to tell random. for python to look inside
+#can import multiple ones by seperating them with coma
+import random,sys,os
+#How to terminate program early
+sys.exit()
+# anything after that wont execute
+#install pyperclip-ha
