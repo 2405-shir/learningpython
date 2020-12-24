fefff3815f4db419cd7af9f222ad050c58bcbce9
shir-bop <73941163+shir-bop@users.noreply.github.com>
2020-12-24 11:39:28 +1000

    Create tip calculator.py

---
+++ day2/tip calculator.py
@@ -0,0 +1,6 @@
+print("welcome to the tip calc")
+x=input("% of tip you would like to give  ")
+y=input("how many peeps will split the bill: ")
+z=x/100
+o=z*y
+print("each person has to pay:" +  str(o))
